5b55d236a913f37a80fb7eab497c74ceeb665c20
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-09-04 12:44:20 +0530

    Setting up the 'auth' route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const pool = require('./db');
+const auth = require('./routes/auth');
 const app = express();
 
 const PORT = process.env.PORT || 8181;
@@ -10,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes:
-
+app.use('/api/auth', auth);
 
 // Starting the server:
 app.listen(PORT, () => {
