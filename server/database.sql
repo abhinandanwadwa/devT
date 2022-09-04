65692c1ba4605c4606f659466d07d2383b74a83c
Abhinandan Wadhwa <abhinandanwadhwa5@gmail.com>
2022-09-04 12:55:04 +0530

    Updating DB Commands

--- server/database.sql
+++ server/database.sql
@@ -2,7 +2,13 @@ CREATE DATABASE devT;
 
 CREATE TABLE users(
     user_id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255),
     email_id VARCHAR(255),
     username VARCHAR(255),
     password VARCHAR(255)
 );
+
+ALTER TABLE users
+ADD COLUMN full_name VARCHAR(255);
+
+DROP TABLE users;
