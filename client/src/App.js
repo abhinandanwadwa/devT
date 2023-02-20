e81dd4238181997d13ec02b93ccf75338fefef60
Abhinandan Wadhwa <74089487+abhinandanwadwa@users.noreply.github.com>
2023-02-20 17:44:17 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 7
+          Learn React with me!!
         </a>
       </header>
     </div>
